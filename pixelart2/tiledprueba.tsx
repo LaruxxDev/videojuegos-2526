e046931278c9be49e09cd1cf28d49f2e23975eaf
lnarote777 <l.narvaez.otero@gmail.com>
2026-03-04 10:41:41 +0100

    tileset (hay que repararlo)

---
+++ pixelart2/tiledprueba.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiledprueba" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="tileset3.png" width="192" height="192"/>
+ <tile id="115">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.2645" y="0.127915">
+    <polygon points="0.72485,-0.0426382 -7.07795,14.8807 0.596935,14.7528"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0426382">
+    <polygon points="0,0 7.88807,14.9234 0.127915,14.966"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
