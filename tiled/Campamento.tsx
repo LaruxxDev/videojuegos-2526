fac225f97e4827f9c23df456d0c3bf80b7eee14c
lnarote777 <nolara@iesrafaelalberti.es>
2026-03-05 13:46:16 +0100

    etrega alejando pixelart

---
+++ tiled/Campamento.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Campamento" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="../pixelart2/campamento.png" width="192" height="192"/>
+ <tile id="115">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9041" y="0.0426382">
+    <polygon points="0,0 0.0426382,14.7955 -7.97335,14.8807 -7.88807,14.028 -0.852765,-0.0852765"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0426382" y="0.0426382">
+    <polygon points="0,0 0.810127,0 7.84544,13.9853 7.93071,14.9234 -0.0426382,14.9234"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
